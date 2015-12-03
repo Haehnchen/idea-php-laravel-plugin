538c5de3a9c7656917c46dae99d4382ce1d6272d
Daniel Espendiller <daniel@espendiller.net>
2015-12-03 20:01:03 +0100

    add travis environment scripts

---
+++ travis.sh
@@ -0,0 +1,119 @@
+#!/bin/bash
+
+ideaVersion="14.1.4"
+if [ "$PHPSTORM_ENV" == "10" ]; then
+    ideaVersion="15.0"
+elif [ "$PHPSTORM_ENV" == "10eap" ]; then
+    ideaVersion="143.870.1"  
+fi
+
+travisCache=".cache"
+
+if [ ! -d ${travisCache} ]; then
+    echo "Create cache" ${travisCache} 
+    mkdir ${travisCache}
+fi
+
+function download {
+
+  url=$1
+  basename=${url##*[/|\\]}
+  cachefile=${travisCache}/${basename}
+  
+  if [ ! -f ${cachefile} ]; then
+      wget $url -P ${travisCache};
+    else
+      echo "Cached file `ls -sh $cachefile` - `date -r $cachefile +'%Y-%m-%d %H:%M:%S'`"
+  fi  
+
+  if [ ! -f ${cachefile} ]; then
+    echo "Failed to download: $url"
+    exit 1
+  fi  
+}
+
+# Unzip IDEA
+
+if [ -d ./idea  ]; then
+  rm -rf idea
+  mkdir idea
+  echo "created idea dir"  
+fi
+
+# Download main idea folder
+download "http://download.jetbrains.com/idea/ideaIU-${ideaVersion}.tar.gz"
+tar zxf ${travisCache}/ideaIU-${ideaVersion}.tar.gz -C .
+
+# Move the versioned IDEA folder to a known location
+ideaPath=$(find . -name 'idea-IU*' | head -n 1)
+mv ${ideaPath} ./idea
+  
+if [ -d ./plugins ]; then
+  rm -rf plugins
+  mkdir plugins
+  echo "created plugin dir"  
+fi
+
+if [ "$PHPSTORM_ENV" == "9" ]; then
+
+    #php
+    download "http://plugins.jetbrains.com/files/6610/20930/php-141.2462.zip"
+    unzip -qo $travisCache/php-141.2462.zip -d ./plugins
+
+    #blade
+    download "http://plugins.jetbrains.com/files/7569/22053/blade-143.381.48.zip"
+    unzip -qo $travisCache/blade-143.381.48.zip -d ./plugins
+
+elif [ "$PHPSTORM_ENV" == "10" ]; then
+
+    #php
+    download "http://plugins.jetbrains.com/files/6610/22045/php-143.381.48.zip"
+    unzip -qo $travisCache/php-143.381.48.zip -d ./plugins
+
+    #blade
+    download "http://plugins.jetbrains.com/files/7569/20766/blade-141.2325.zip"
+    unzip -qo $travisCache/blade-141.2325.zip -d ./plugins
+
+elif [ "$PHPSTORM_ENV" == "10eap" ]; then
+
+    #php
+    download "http://plugins.jetbrains.com/files/6610/22267/php-143.790.zip"
+    unzip -qo $travisCache/php-143.790.zip -d ./plugins
+
+    #blade
+    download "http://plugins.jetbrains.com/files/7569/20766/blade-141.2325.zip"
+    unzip -qo $travisCache/blade-141.2325.zip -d ./plugins
+    
+elif [ "$PHPSTORM_ENV" == "eap" ]; then
+
+    # TODO: extract latest builds for plugins from eap site they are not public
+    # https://confluence.jetbrains.com/display/PhpStorm/PhpStorm+Early+Access+Program
+    echo "No configuration for PhpStorm: $PHPSTORM_ENV"
+    exit 1
+
+else
+    echo "Unknown PHPSTORM_ENV value: $PHPSTORM_ENV"
+    exit 1
+fi
+
+# Run the tests
+if [ "$1" = "-d" ]; then
+    ant -d -f build-test.xml -DIDEA_HOME=./idea
+else
+    ant -f build-test.xml -DIDEA_HOME=./idea
+fi
+
+# Was our build successful?
+stat=$?
+
+if [ "${TRAVIS}" != true ]; then
+    ant -f build-test.xml -q clean
+
+    if [ "$1" = "-r" ]; then
+        rm -rf idea
+        rm -rf plugins
+    fi
+fi
+
+# Return the build status
+exit ${stat}
