6d0807096be5a93130257d36f3834d5fd4a93ab9
Adel <adel.faiz@gmail.com>
2017-01-21 17:50:17 +0300

    Enable extract partial view action only for selected text

--- src/de/espend/idea/laravel/blade/actions/ExtractPartialViewAction.java
+++ src/de/espend/idea/laravel/blade/actions/ExtractPartialViewAction.java
@@ -1,6 +1,9 @@
 package de.espend.idea.laravel.blade.actions;
 
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.CommonDataKeys;
 import com.intellij.openapi.actionSystem.DataContext;
+import com.intellij.openapi.editor.Editor;
 import com.intellij.psi.PsiElement;
 import com.intellij.refactoring.RefactoringActionHandler;
 import com.intellij.refactoring.actions.BaseRefactoringAction;
@@ -29,5 +32,16 @@ public class ExtractPartialViewAction extends BaseRefactoringAction {
 
     protected boolean isAvailableForLanguage(com.intellij.lang.Language language) {
         return BladeLanguage.INSTANCE.getID().equals(language.getID());
+    }
+
+    @Override
+    public void update(AnActionEvent anActionEvent) {
+        super.update(anActionEvent);
+
+        Editor editor = anActionEvent.getData(CommonDataKeys.EDITOR);
+
+        if(editor == null) return;
+
+        anActionEvent.getPresentation().setEnabled((editor.getSelectionModel().hasSelection()));
     }
 }
