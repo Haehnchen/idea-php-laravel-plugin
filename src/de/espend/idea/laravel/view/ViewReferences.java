e952d2d352c3cc3ebbccc8be0459662bea4f79bd
Diogo Gomes <diogogomeswww@gmail.com>
2017-12-01 10:41:11 +0000

    merge conflicts

--- src/de/espend/idea/laravel/view/ViewReferences.java
+++ src/de/espend/idea/laravel/view/ViewReferences.java
@@ -44,10 +44,6 @@ public class ViewReferences implements GotoCompletionRegistrar {
         new MethodMatcher.CallToSignature("\\Illuminate\\View\\Factory", "renderEach"),
         new MethodMatcher.CallToSignature("\\Illuminate\\View\\Factory", "callComposer"),
         new MethodMatcher.CallToSignature("\\Illuminate\\View\\Factory", "callCreator"),
-
-        new MethodMatcher.CallToSignature("\\Illuminate\\Mail\\Mailer", "send"),
-        new MethodMatcher.CallToSignature("\\Illuminate\\Mail\\Mailer", "plain"),
-        new MethodMatcher.CallToSignature("\\Illuminate\\Mail\\Mailer", "queue"),
     };
 
     @Override
